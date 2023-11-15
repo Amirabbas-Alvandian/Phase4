af3c0c35152103cfea3338ca4da4868804aa1a64
Amirabbas-Alvandian <alvandiyan77@gmail.com>
2023-11-15 18:15:00 +0330

    EmailConfig

---
+++ src/main/java/com/example/phase4/entity/ConfirmationToken.java
@@ -0,0 +1,41 @@
+package com.example.phase4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="token_id")
+    private long tokenid;
+
+    @Column(name="confirmation_token")
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public ConfirmationToken(User user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+
+    // getters and setters
+}
